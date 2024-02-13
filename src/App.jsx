61dd159574f3a2d16e377298de9c88cb93d8d8c1
AngerOverApathy <violasian@gmail.com>
2024-02-12 20:50:01 -0800

    styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import Menu from './components/menu/Menu'
 import { GameStateContext } from './helpers/Contexts'
 import './App.css'
