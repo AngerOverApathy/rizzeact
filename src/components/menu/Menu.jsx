51a6f082e8518cae02b66749666f70d4f957e0ea
AngerOverApathy <violasian@gmail.com>
2024-02-22 20:01:00 -0800

    styling added

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,6 +4,7 @@ export default function Menu () {
   return (
     <>
     <div className="Menu">
+        <label>Enter Your Name:</label>
         <input type="text" placeholder="Your Response"/>
         <button>Start Quiz</button>
     </div>
