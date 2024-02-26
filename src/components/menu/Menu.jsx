13fb8268f4224320272499094136e64b6f885895
AngerOverApathy <violasian@gmail.com>
2024-02-25 19:55:25 -0800

    styling added

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -2,9 +2,11 @@ import './index.css'
 
 export default function Menu () {
   return (
+    <>
     <div className="Menu">
-      <input type="text" placeholder=" Your Response"/>
-      <button>Start Quiz</button>
+        <input type="text" placeholder="Your Response"/>
+        <button>Start Quiz</button>
     </div>
+    </>
   )
 }
