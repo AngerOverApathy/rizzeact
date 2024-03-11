d1e00268aa892852208832bcea47d9dc89e68a0a
AngerOverApathy <violasian@gmail.com>
2024-03-11 13:55:10 -0700

    question component created

--- src/helpers/Questions.jsx
+++ src/helpers/Questions.jsx
@@ -1,3 +1,15 @@
 
 
-const Questions = [{}, {}]
+const Questions = [
+    {
+        prompt: 'What is 1+3?',
+        optionA: '24',
+        optionB: '21',
+        optionC: '4',
+        optionD: '3',
+        answer: 'C'
+    }, 
+    {
+
+    }
+]
