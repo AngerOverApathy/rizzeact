8d1040ecf919ed1470d330b5453a671605de0c7e
AngerOverApathy <violasian@gmail.com>
2024-03-14 16:07:17 -0700

    question component created

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,10 +1,19 @@
-import './index.css'
+import { useState } from 'react'
 import { Questions } from '../helpers/Questions'
+import './index.css'
 
 export default function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+
   return (
     <div className="Quiz">
-      <h1>Quiz</h1>
+      <h1>{Questions[currentQuestion].prompt}</h1>
+        <div className="questions">
+            <button>{Questions[currentQuestion].optionA}</button>
+            <button>{Questions[currentQuestion].optionB}</button>
+            <button>{Questions[currentQuestion].optionC}</button>
+            <button>{Questions[currentQuestion].optionD}</button>
+        </div>
     </div>
   )
 }
