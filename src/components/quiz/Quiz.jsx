b7f43bd183ed8639c69160320fffc39fa7e16796
AngerOverApathy <violasian@gmail.com>
2024-03-15 16:08:59 -0700

    question component created

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -3,13 +3,18 @@ import { Questions } from '../helpers/Questions'
 import './index.css'
 
 export default function Quiz() {
-  const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [optionChosen, setOptionChosen] = useState('')
+
+    const chooseOption = (option) => {
+        setOptionChosen(option)
+    }
 
   return (
     <div className="Quiz">
       <h1>{Questions[currentQuestion].prompt}</h1>
         <div className="questions">
-            <button>{Questions[currentQuestion].optionA}</button>
+            <button onClick={}>{Questions[currentQuestion].optionA}</button>
             <button>{Questions[currentQuestion].optionB}</button>
             <button>{Questions[currentQuestion].optionC}</button>
             <button>{Questions[currentQuestion].optionD}</button>
