07b1a93886e389e28bba3577d6a55cc062fc8fef
AngerOverApathy <violasian@gmail.com>
2024-03-18 13:41:09 -0700

    onchange event added

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import './App.css'
 
 function App() {
   const [gameState, setGameState] = useState('menu')
+  const [userName, setUserName] = useState('')
 
   return (
     <>
     <div className='App'>
         <h1>Quiz App</h1>
-          <GameStateContext.Provider value={{ gameState, setGameState }}>
+          <GameStateContext.Provider value={{ gameState, setGameState, userName, setUserName }}>
             {gameState === 'menu' && <Menu />}
           </GameStateContext.Provider>
     </div>
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -3,13 +3,15 @@ import { GameStateContext } from '../../helpers/Contexts'
 import './index.css'
 
 export default function Menu () {
-  const { gameState, setGameState } = useContext(GameStateContext)
+  const { gameState, setGameState, userName, setUserName } = useContext(GameStateContext)
 
   return (
     <>
     <div className="Menu">
         <label>Enter Your Name:</label>
-        <input type="text" placeholder="Your Response"/>
+        <input type="text" placeholder="Your Response" onChange={(event)=>{
+          setUserName(event.target.value)
+        }}/>
           <button 
             onClick={()=>{
             setGameState('playing')
